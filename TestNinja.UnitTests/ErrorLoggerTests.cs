612a67e9c15379bbeeacae9551712b747fa0ed94
Tom <tom.krut@gmail.com>
2021-02-16 21:07:44 +0000

    Test method that returns void but changes property in the class

---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class ErrorLoggerTests
+    {
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            var logger = new ErrorLogger();
+
+            logger.Log("a"); //This method returns void, hoever it changes the "LastError" property in the class
+                             //and we can test against this:
+            Assert.That(logger.LastError, Is.EqualTo("a"));
+        }
+    }
+}
