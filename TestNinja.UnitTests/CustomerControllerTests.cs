36294f28268291ac306e1962c0c905bb3a743385
Tom <tom.krut@gmail.com>
2021-02-16 20:49:58 +0000

    Add tests for methods that return instances of  different classes

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFoud()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(0);
+
+            Assert.That(result, Is.TypeOf<NotFound>()); //TypeOf means given class ONLY
+            //or 
+            
+            //Assert.That(result, Is.InstanceOf<NotFound>()); //InstanceOf means instance of given class OR one of it's derivatives, 
+                                                            //so below will work fine:
+            //Assert.That(result, Is.InstanceOf<ActionResult>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsNotZero_ReturnOk()
+        {
+            var controller = new CustomerController();
+
+            var result = controller.GetCustomer(1);
+
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
