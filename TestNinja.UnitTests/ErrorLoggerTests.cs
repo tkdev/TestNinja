659db1d72abaf76ac9f167786b3f9ea1805d7306
Tom <tom.krut@gmail.com>
2021-02-17 11:21:38 +0000

    Testing methods that raise an event

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -33,5 +33,18 @@ namespace TestNinja.UnitTests
             // to test throwing exception we use lambda (delegate):
             Assert.That(() => logger.Log(errorMsg), Throws.ArgumentNullException); 
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
     }
 }
